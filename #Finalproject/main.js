c2856ef72c96b1e8fd52eda0d1e29f34e59ba2c9
itmecat2058 <jmorley2007@gmail.com>
2024-12-20 08:38:45 -0500

    Added to style and html

--- #Finalproject/main.js
+++ #Finalproject/main.js
@@ -4,21 +4,26 @@ const searchForm = document.querySelector(".searchForm")
 const searchInput = document.querySelector(".searchInput")
 
 searchForm.addEventListener("keyup", () => {
-    
+    let searchTerm = searchInput.value.toLowerCase();
+    filteredProducts = products.filter(product => {
+        return product.title.includes(searchTerm)
+    })
+    showProd()
 })
 
 function showProd() {
-    productsContainer.innerHTML = products.map(product => {
+    productsContainer.innerHTML = filteredProducts.map(product => {
         return `
                 <article class="product">
-                    <img src="${product.image}" alt="${product.title}">
+                    <img src="${product.image}" alt="${product.title}" class="productImage">
                     <footer>
                         <p class="productName">${product.title}</p>
                         <p class="productPrice">${product.price}</p>
+                        <button class="btn">Add to Cart</button>
                     </footer>
                 </article>
                 `
-    })
+    }).join("")
 }
 
 showProd()
