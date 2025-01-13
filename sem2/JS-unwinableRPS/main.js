802b21ea3beb39964597df7f8ef175344b95a5b6
itmecat2058 <jmorley2007@gmail.com>
2025-01-13 11:36:22 -0500

    Added unrigged rps

--- sem2/JS-unwinableRPS/main.js
+++ sem2/JS-unwinableRPS/main.js
@@ -1,21 +1,75 @@
-let CompChoice = document.getElementById("compChoice")
+let CompChoiceElem = document.getElementById("compChoice")
 let iconImage = document.querySelector("#iconImage")
-let PlayerChoice = null
+let formSub = document.getElementById("form")
+let formInput = document.getElementById("formInp")
+let compChoice = ["rock", "paper", "scissors"]
+let toggleBtn = document.getElementById("toggleBtn")
 
-function play() {
-    let PlayerChoice = prompt("rock? paper? scissors?")
+let isRigged = true
 
+let PlayerChoice;
+
+function randChoice() {
+    return Math.floor(Math.random() * compChoice.length)
+}
+
+//playchoice
+toggleBtn.addEventListener("click", function () {
+    isRigged = !isRigged
+    if (isRigged) {
+        toggleBtn.innerText = "Rigged"
+    } else {
+        toggleBtn.innerText = "Not Rigged"
+    }
+})
+
+function toggle(PlayerChoice){
+    if (isRigged == true) {
+        play(PlayerChoice)
+    } else {
+        play2(PlayerChoice)
+    }
+}
+
+//input
+formSub.addEventListener("submit", function(event) {
+    event.preventDefault()
+    let PlayerChoice = String(formInput.value)
+    toggle(String(PlayerChoice))
+})
+
+//rigged
+function play(PlayerChoice) {
     if (String(PlayerChoice) == "paper") {
-        CompChoice.innerText = "Scissors"
+        CompChoiceElem.innerText = "Scissors"
         iconImage.src = "images/scissors.png"
     } else if (String(PlayerChoice) == "rock") {
-        CompChoice.innerText = "Paper"
+        CompChoiceElem.innerText = "Paper"
         iconImage.src = "images/paper.png"
     } else if (String(PlayerChoice) == "scissors"){
-        CompChoice.innerText = "Rock"
+        CompChoiceElem.innerText = "Rock"
         iconImage.src = "images/rock.png"
     } else {
-        CompChoice.innerText = "You idiot i said pick ROCK, PAPER, OR SCISSORS!!!"
+        CompChoiceElem.innerText = "You idiot i said pick ROCK, PAPER, OR SCISSORS!!!"
         iconImage.src = "images/dumb.png"
+    }
+}
+
+
+//unrigged
+function play2(PlayerChoice) {
+    let compThrow = compChoice[randChoice()]
+    if (PlayerChoice != "rock" && PlayerChoice != "paper" && PlayerChoice != "scissors") {
+        CompChoiceElem.innerText = "You idiot i said pick ROCK, PAPER, OR SCISSORS!!!"
+        iconImage.src = "images/dumb.png"
+    } else if (PlayerChoice == "rock" && compThrow == "paper" || PlayerChoice == "paper" && compThrow == "scissors" || PlayerChoice == "scissors" && compThrow == "rock") {
+        CompChoiceElem.innerText = `You lose, I chose ${compThrow}`
+        iconImage.src = `images/${compThrow}.png`
+    } else if (compThrow == PlayerChoice) {
+        CompChoiceElem.innerText = `Its a tie, I chose ${compThrow}`
+        iconImage.src = "images/tie.png"
+    } else {
+        CompChoiceElem.innerText = `You win, I chose ${compThrow}`
+        iconImage.src = "images/win.png"
     }
 }
